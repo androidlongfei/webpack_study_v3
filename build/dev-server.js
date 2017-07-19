d5bbdbbf9f003762d30660e31a6ac8bc85de481e
local_author <androidlongfei@163.com>
2017-07-20 07:48:29 +0800

    add build

---
+++ build/dev-server.js
@@ -0,0 +1,70 @@
+var config = require('../config')
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
+var opn = require('opn')
+var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = require('./webpack.config.dev')
+
+var port = 9090
+// Define HTTP proxies to your custom API backend
+// https://github.com/chimurai/http-proxy-middleware
+
+var server = express()
+var compiler = webpack(webpackConfig)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+        colors: true,
+        chunks: false
+    }
+})
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler)
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+        hotMiddleware.publish({
+            action: 'reload'
+        })
+        cb()
+    })
+})
+
+var context = config.dev.context
+
+var proxypath = 'http://localhost:8001'
+
+var options = {
+    target: proxypath,
+    changeOrigin: true,
+}
+if (context.length) {
+    server.use(proxyMiddleware(context, options))
+}
+
+// handle fallback for HTML5 history API
+server.use(require('connect-history-api-fallback')())
+
+// serve webpack bundle output
+server.use(devMiddleware)
+
+// enable hot-reload and state-preserving
+// compilation error display
+server.use(hotMiddleware)
+
+// serve pure static assets
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+server.use(staticPath, express.static('./static'))
+
+module.exports = server.listen(port, function(err) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    var uri = 'http://localhost:' + port
+    console.log('Listening at ' + uri + '\n')
+
+    opn(uri)
+})
---
+++ build/webpack.config.base.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack')
+var path = require('path')
+var config = require('../config')
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        publicPath: config.dev.assetsPublicPath,
+        path: config.dev.assetsRoot, // 目标文件路径
+        filename: '[name].js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                include: path.resolve(__dirname, '../'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true
+                    }
+                }],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: path.posix.join(config.dev.assetsSubDirectory, 'images/[name].[ext]')
+                    }
+                }]
+            }
+        ],
+    }
+}
